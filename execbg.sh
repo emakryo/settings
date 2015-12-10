f0bc46e20731ab09b05abf2c6016c22dc6617bc3
Kamesawa Ryosuke <emak.ryo@gmail.com>
2015-12-10 17:00:58 +0900

    fix not to show slash in log/err file name

--- execbg.sh
+++ execbg.sh
@@ -2,11 +2,12 @@
 # Usage: ./execbg.sh [command]
 # you can include input redirect but must not include output redirect
 
-file=`echo $* | sed -e "s/ /_/g"`_`date +"%F_%T"`
-#echo $file
+file=`echo $* | sed -e "s/ /_/g" -e "s/\//_/g"`_`date +"%F_%T"`
+echo $file
 if [ `echo "$*" | grep ">"` ]
 then
     echo "No output redirection"
+    exit 1
 fi
 
 if [ `echo "$*" | grep "<"` ]
