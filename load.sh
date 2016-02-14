32f4cb3010179442656b38a4e5103d71d307da3e
Kamesawa Ryosuke <emak.ryo@gmail.com>
2016-02-14 19:15:43 +0900

    fix bug

--- load.sh
+++ load.sh
@@ -3,7 +3,7 @@
 
 SCRIPT_DIR=$(cd "$(dirname $0)" && pwd)
 echo $SCRIPT_DIR
-[ -f $HOME/.bashrc ] && mv $HOME/.bashrc $HOME/.bashrc_local;
+[ -f $HOME/.bashrc ] && cat $HOME/.bashrc >> $HOME/.bashrc_local && rm $HOME/.bashrc
 ln -s $SCRIPT_DIR/.bashrc $HOME/.bashrc
-[ ! -d $HOME/.emasc.d ] && mkdir $HOME/.emasc.d
-ln -s $SCRIPT_DIR/init.el $HOME/.emasc.d/init.el
+[ ! -d $HOME/.emacs.d ] && mkdir $HOME/.emacs.d
+ln -s $SCRIPT_DIR/init.el $HOME/.emacs.d/init.el
