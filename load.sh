61c7598845bafb79b5d6d0c916fc23a3b56a59be
Ryosuke Kamesawa <emak.ryo@gmail.com>
2017-04-06 16:23:34 +0900

    Modify load.sh

--- load.sh
+++ load.sh
@@ -4,17 +4,21 @@
 SCRIPT_DIR=$(cd "$(dirname $0)" && pwd)
 echo $SCRIPT_DIR
 
+git submodule init
+git submodule update
+
 # load bashrc in settings
-echo "source $SCRIPT_DIR/bashrc" >> $HOME/.bashrc
+echo ". $SCRIPT_DIR/bashrc" >> $HOME/.bashrc
 
 # load emacs init.el
-[ ! -d $HOME/.emacs.d ] && mkdir $HOME/.emacs.d
-ln -s $SCRIPT_DIR/init.el $HOME/.emacs.d/init.el
+#[ ! -d $HOME/.emacs.d ] && mkdir $HOME/.emacs.d
+#ln -s $SCRIPT_DIR/init.el $HOME/.emacs.d/init.el
 
 # load vim settings
 ln -s $SCRIPT_DIR/.vimrc $HOME
 ln -s $SCRIPT_DIR/.vim $HOME
+vim -c "PlugInstall" -c "quit"
 
 # load byobu settings
-[ -f $HOME/.byobu/.tmux.conf ] && mv $HOME/.byobu/.tmux.conf $HOME/.byobu/.tmux.conf.bak
+[ -f $HOME/.byobu/.tmux.conf ] && mv $HOME/.byobu/.tmux.conf $HOME/.byobu/.tmux.conf.old
 [ -d $HOME/.byobu ] && ln -s $SCRIPT_DIR/.tmux.conf $HOME/.byobu/
